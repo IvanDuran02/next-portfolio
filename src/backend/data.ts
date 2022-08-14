c5f3d8dc45ad7051da349973e41aaad073dcae8a
Ivan Duran <iamivanduran02@gmail.com>
2022-08-14 15:16:43 -0400

    refined card UI, added headlines to card data, added a read more and hide collapsible description. forgot what else I did

--- src/backend/data.ts
+++ src/backend/data.ts
@@ -8,8 +8,9 @@ const Cards = {
     {
       id: 1,
       title: "Glass Design Project",
+      headline: "Created with - Html, CSS, Javascript",
       description:
-        "Created with - Html, CSS, Javascript\nSimple design project with no functionality. I made it because I wanted to familiarize myself with the glass morphism design style.",
+        "Simple design project with no functionality. I made it because I wanted to familiarize myself with the glass morphism design style.",
       image:
         "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FglassProject.133bf4ff.png&w=3840&q=75",
       link: "https://glasswebsite.vercel.app/",
@@ -17,8 +18,9 @@ const Cards = {
     {
       id: 2,
       title: "Slagville",
+      headline: "Created with - React, Tailwind, Typescript, MongoDB",
       description:
-        "Created with - React, Tailwind, Typescript, MongoDB\nProject I made for fun to show some friends. For the most part its a static blog-like site, but I was a bit more ambitious with the project and added a chatting functionality. I used a database to store messages from a post form on the front-end. I also Discord for OAuth2 authentication.",
+        "Project I made for fun to show some friends. For the most part its a static blog-like site, but I was a bit more ambitious with the project and added a chatting functionality. I used a database to store messages from a post form on the front-end. I also Discord for OAuth2 authentication.",
       image:
         "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FslagvilleProject.669fb9d5.png&w=3840&q=75",
       link: "https://slagville.vercel.app/",
@@ -26,8 +28,9 @@ const Cards = {
     {
       id: 3,
       title: "Create React Project",
+      headline: "Created with - React",
       description:
-        "Created with - React\nOne of the first sites I made with React following a tutorial.",
+        "One of the first sites I made with React following a tutorial.",
       image:
         "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FwujuProject.ebac9899.png&w=3840&q=75",
       link: "https://wujulikeacoach.vercel.app/",
@@ -35,8 +38,9 @@ const Cards = {
     {
       id: 4,
       title: "Medium Clone",
+      headline: "Created with - React, TypeScript, Tailwind, SanityCMS",
       description:
-        "Created with - React, TypeScript, Tailwind, SanityCMS\nClone of the Medium blogging site that uses Sanity.io as a CMS.",
+        "Clone of the Medium blogging site that uses Sanity.io as a CMS.",
       image:
         "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FmediumProject.589900d1.png&w=3840&q=75",
       link: "https://ivan-medium-clone.vercel.app/",
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,29 +1,60 @@
 /* eslint-disable @next/next/no-img-element */
 import Cards from "../backend/data";
 import Link from "next/link";
+import { useState } from "react";
 function ProjectPage() {
   // http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FslagvilleProject.669fb9d5.png&w=3840&q=75 slagville image link
   // http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FglassProject.133bf4ff.png&w=3840&q=75 glass image link
   // http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FmediumProject.589900d1.png&w=3840&q=75 medium image link
   // http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FwujuProject.ebac9899.png&w=3840&q=75 wuju image link
 
+  // onClick open the description of the project
+
+  const [toggle, setToggle] = useState(0);
+
+  const clickHandler = (e: any, projectID: any) => {
+    if (toggle === projectID) {
+      setToggle(0);
+      return;
+    }
+    setToggle(projectID);
+  };
+
   return (
-    <div className="bg-[#171717] w-screen h-auto flex justify-center items-center text-white flex-wrap text-center content-center">
-      <div className="max-w-[80%] flex flex-wrap justify-center">
+    <div className="bg-[#171717] w-screen h-auto flex justify-center items-center flex-wrap transition-all">
+      <div className="max-w-[1500px] flex flex-wrap justify-center">
         {Cards.ProjectCards.map((card) => (
           <div key={card.id}>
-            <Link href={card.link} target="_blank">
-              <div className="flex flex-col m-8 hover:scale-110 cursor-pointer transition-all w-[420px] h-[500px] justify-start items-center text-center ">
-                <h1 className="py-4 font-bold">{card.title}</h1>
-                <div className="p-2" />
-                <img
-                  src={card.image}
-                  alt={card.title}
-                  className="w-[341px] h-48"
-                />
-                <p className="p-4 pt-4">{card.description}</p>
+            <div className="bg-white flex flex-col m-8 hover:scale-105 cursor-pointer transition-all w-[420px] justify-start items-center text-center p-4 h-auto min-h-[450px] overflow-hidden border-2 rounded-md bg-clip-text bg-gradient-to-r from-t-pink via-t-purple to-t-orange animate-gradient-text">
+              <Link href={card.link} target="_blank">
+                <h1 className="pt-2 font-bold text-center text-transparent bold-text bg-clip-text bg-gradient-to-r from-t-pink via-t-purple to-t-orange animate-gradient-text py-4">
+                  {card.title}
+                </h1>
+              </Link>
+
+              <img
+                src={card.image}
+                alt={card.title}
+                className="w-[432px] h-[243] rounded-md"
+              />
+              <div className="max-w-[430px] flex-grow">
+                {toggle === card.id ? (
+                  <div className="p-4 pt-4 text-transparent">
+                    {card.description}
+                  </div>
+                ) : (
+                  <div className="p-2 pt-4 font-bold text-red-400 min-h-[6rem]">
+                    {card.headline}
+                  </div>
+                )}
+                <div
+                  onClick={(e) => clickHandler(e, card.id)}
+                  className="pt-2 font-semibold text-center text-transparent bold-text bg-clip-text bg-gradient-to-r from-t-pink via-t-purple to-t-orange animate-gradient-text"
+                >
+                  {toggle === card.id ? `Hide...` : `Read more...`}
+                </div>
               </div>
-            </Link>
+            </div>
           </div>
         ))}
       </div>
