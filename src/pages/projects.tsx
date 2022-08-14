c2da661910e5f0789562aae7d8d08f99b10f1e43
Ivan Duran <iamivanduran02@gmail.com>
2022-08-14 01:04:32 -0400

    added next lint, links open new tab now, fixed resolution on project images

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,10 +1,7 @@
-import React from "react";
+/* eslint-disable @next/next/no-img-element */
 import Cards from "../backend/data";
-import slagville from "../../public/wujuProject.png";
-import Image from "next/image";
 import Link from "next/link";
 function ProjectPage() {
-  console.log(Cards.ProjectCards[1].image);
   // http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FslagvilleProject.669fb9d5.png&w=3840&q=75 slagville image link
   // http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FglassProject.133bf4ff.png&w=3840&q=75 glass image link
   // http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FmediumProject.589900d1.png&w=3840&q=75 medium image link
@@ -15,11 +12,15 @@ function ProjectPage() {
       <div className="max-w-[80%] flex flex-wrap justify-center">
         {Cards.ProjectCards.map((card) => (
           <div key={card.id}>
-            <Link href={card.link}>
+            <Link href={card.link} target="_blank">
               <div className="flex flex-col m-8 hover:scale-110 cursor-pointer transition-all w-[420px] h-[500px] justify-start items-center text-center ">
                 <h1 className="py-4 font-bold">{card.title}</h1>
                 <div className="p-2" />
-                <img src={card.image} alt={card.title} className="w-96 h-48" />
+                <img
+                  src={card.image}
+                  alt={card.title}
+                  className="w-[341px] h-48"
+                />
                 <p className="p-4 pt-4">{card.description}</p>
               </div>
             </Link>
