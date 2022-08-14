e6966f87c74278406d0934fb30537c19e2315238
Ivan Duran <iamivanduran02@gmail.com>
2022-08-14 15:23:38 -0400

    Just added a work in progress note for my vercel deployment

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,6 +8,9 @@ function HeroSection() {
         <h1 className="pt-2 text-4xl font-bold text-center text-transparent bold-text bg-clip-text bg-gradient-to-r from-t-pink via-t-purple to-t-orange animate-gradient-text py-4">
           Welcome to my Portfolio!
         </h1>
+        <h3 className="text-white opacity-80">
+          Note: Currently a work in progress but check out my Projects tab!
+        </h3>
       </div>
     </>
   );
