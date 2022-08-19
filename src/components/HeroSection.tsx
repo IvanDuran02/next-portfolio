fe2addf932ba307fe489f6b02b386e7cff796aa3
Ivan Duran <iamivanduran02@gmail.com>
2022-08-18 20:02:00 -0400

    Fixed sizes for mobile(made text a bit smaller for navbar and landing page)

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,12 +5,12 @@ function HeroSection() {
     <>
       <div className="bg-[#171717] w-screen h-screen flex flex-col justify-inbetween items-center">
         <div className="pt-8" />
-        <h1 className="pt-2 text-4xl font-bold text-center text-transparent bold-text bg-clip-text bg-gradient-to-r from-t-pink via-t-purple to-t-orange animate-gradient-text py-4">
+        <h1 className="pt-2 px-4 text-3xl font-bold text-center text-transparent bold-text bg-clip-text bg-gradient-to-r from-t-pink via-t-purple to-t-orange animate-gradient-text py-4">
           Welcome to my Portfolio!
         </h1>
-        <h3 className="text-white opacity-80">
+        <p className="text-white opacity-80 text-center px-6 text-xs">
           Note: Currently a work in progress but check out my Projects tab!
-        </h3>
+        </p>
       </div>
     </>
   );
